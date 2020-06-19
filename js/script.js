f1044e17996db59655f204b6b54ab1235a58604c
Klimov-V <klimov_vitaliy@icloud.com>
2020-06-19 15:58:43 +0300

    added adaptive menu, finished header

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 (() => {
     'use strict'
 
+    let toggleMunu = document.querySelector('.toggleMenu');
+
+    toggleMunu.addEventListener('click', (e) => {
+        e.preventDefault();
+        let menu = document.querySelector('.header__menu');
+
+        menu.classList.toggle('active');
+    });
     
     
 })();
