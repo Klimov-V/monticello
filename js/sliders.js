ecd61b9f896fdc3bf70b171455b324c19380f001
Klimov-V <klimov_vitaliy@icloud.com>
2020-06-21 02:58:36 +0300

    stгсk on news

--- js/sliders.js
+++ js/sliders.js
@@ -1,6 +1,18 @@
-var mySwiper = new Swiper ('.swiper-container', {
+var aboutSwiper = new Swiper ('.about__swiper', {
     direction: 'vertical',
     loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        bulletClass: 'slider__dot',
+        bulletActiveClass: 'slider__dot--active'
+    },
+})
+
+var newsSwiper = new Swiper ('.news__swiper', {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 30,
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
