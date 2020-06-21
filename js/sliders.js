fa4f2d9b93191badc27e06f35ced656535142183
Klimov-V <klimov_vitaliy@icloud.com>
2020-06-21 18:14:31 +0300

    finised gallery

--- js/sliders.js
+++ js/sliders.js
@@ -16,12 +16,16 @@ var newsSwiper = new Swiper ('.news__swiper', {
     autoplay: {
         delay: 4000,
         disableOnInteraction: false,
-      },
+    },
     spaceBetween: 30,
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
         bulletClass: 'slider__dot',
         bulletActiveClass: 'slider__dot--active'
+    },
+    navigation: {
+        nextEl: '.news__next',
+        prevEl: '.news__prev',
     },
 })
