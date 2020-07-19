e8426b9f99e587fa85ea8f51078f050c94966be4
Klimov-V <klimov_vitaliy@icloud.com>
2020-07-19 13:59:20 +0300

    changed links to swiper, created task for js

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,10 @@ var browserSync	 = require('browser-sync').create();
 var sass       	 = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps   = require('gulp-sourcemaps');
+var rigger       = require('gulp-rigger');
+const babel      = require('gulp-babel');
+var rename       = require('gulp-rename');
+var uglify       = require('gulp-uglify-es').default;
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
@@ -19,8 +23,24 @@ gulp.task('sass', function() {
         .pipe(browserSync.stream());
 });
 
+gulp.task('js', function() {
+    return gulp.src('js/main.js')
+        .pipe(rigger())
+        .pipe(sourcemaps.init())
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(uglify(/* options */))
+        .pipe(rename({
+            suffix: ".min"
+          }))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('js'))
+        .pipe(browserSync.stream());
+})
+
 // Static Server + watching scss/html files
-gulp.task('serve', gulp.series('sass', function() {
+gulp.task('serve', gulp.series('sass', 'js', function() {
 
     browserSync.init({
         server: "./"
@@ -28,7 +48,7 @@ gulp.task('serve', gulp.series('sass', function() {
 
     gulp.watch("scss/*.scss", gulp.parallel('sass'));
     gulp.watch("*.html").on('change', browserSync.reload);
-    gulp.watch("js/*.js").on('change', browserSync.reload);
+    gulp.watch("js/**/*.js").on('change', browserSync.reload);
 }));
 
 
---
+++ js/main.js
@@ -0,0 +1,5 @@
+//= headroom.min.js
+//= header.js
+//= sliders.js
+//= script.js
+//= mapStyles.js
